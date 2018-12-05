abbccdd5f00f95e65a16a4f4d4345a226b58f8eb
Vic Black <vic_black@163.com>
2018-12-06 00:08:56 +0800

    实现功能和进度条

--- FolderDivider/MainWindow.xaml.cs
+++ FolderDivider/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,8 +26,10 @@ namespace FolderDivider
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Topmost = true;
             _Model = new Model();
-            this.Content = _Model;
+            this.DataContext = _Model;
         }
 
         private void On_SetInputPath_CanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -39,24 +44,55 @@ namespace FolderDivider
 
         private void On_Divide_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = _Model != null;
+            e.CanExecute = _Model != null && _Model.Nums>0 && !String.IsNullOrEmpty(_Model.InputPath) && !String.IsNullOrEmpty(_Model.OutPath);
         }
 
         private void OnSetInputPath_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            System.Windows.Forms.FolderBrowserDialog folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                _Model.InputPath = folderBrowserDialog.SelectedPath;
+            }
         }
 
         private void OnSetOutPath_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            System.Windows.Forms.FolderBrowserDialog folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                _Model.OutPath = folderBrowserDialog.SelectedPath;
+            }
         }
 
         private void OnDivide_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            _Model.filelist = new List<string>(_Model.TotalNums);
+            _Model.GetFilesName(_Model.InputPath);
+            int[] array = new int[_Model.TotalNums];
+            double progs;
+            _Model.GetRandomByGuid(array);
+            string outpath1 = _Model.OutPath + @"\1_" + _Model.Percent.ToString();
+            string outpath2 = _Model.OutPath + @"\2_" + (100 - _Model.Percent).ToString();
+            Directory.CreateDirectory(outpath1);
+            Directory.CreateDirectory(outpath2);
+            for (int i = 0; i < _Model.Nums; i++)
+            {
+                File.Copy(_Model.filelist[i].ToString(), outpath1 + @"\" + System.IO.Path.GetFileName(_Model.filelist[i].ToString()), true);
+                progs = i * 100.0 / _Model.TotalNums;
+                progressbar.Dispatcher.Invoke(new Action<System.Windows.DependencyProperty, object>(progressbar.SetValue), System.Windows.Threading.DispatcherPriority.Background, ProgressBar.ValueProperty, progs);
+            }
+            for (int i = _Model.Nums; i < _Model.TotalNums; i++)
+            {
+                File.Copy(_Model.filelist[i].ToString(), outpath2 + @"\" + System.IO.Path.GetFileName(_Model.filelist[i].ToString()), true);
+                progs = i * 100.0 / _Model.TotalNums;
+                progressbar.Dispatcher.Invoke(new Action<System.Windows.DependencyProperty, object>(progressbar.SetValue), System.Windows.Threading.DispatcherPriority.Background, ProgressBar.ValueProperty, progs);
+            }
+            progressbar.Dispatcher.Invoke(new Action<System.Windows.DependencyProperty, object>(progressbar.SetValue), System.Windows.Threading.DispatcherPriority.Background, ProgressBar.ValueProperty, 0.0);
+            MessageBox.Show("操作完成", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-
         private Model _Model;
     }
 }
--- FolderDivider/Model.cs
+++ FolderDivider/Model.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FolderDivider
 {
-    class Model
+    class Model : INotifyPropertyChanged
     {
+        #region 成员
+        public string InputPath { get { return _InputPath; } set { if (_InputPath == value) return; _InputPath = value; _TotalNums = GetFilesCount(InputPath); OnPropertyChanged(nameof(TotalNums)); OnPropertyChanged(nameof(InputPath)); } }
+        private string _InputPath;
+
+        public string OutPath { get { return _OutPath; } set { if (_OutPath == value) return; _OutPath = value; OnPropertyChanged(nameof(OutPath)); } }
+        private string _OutPath;
+
+        public int TotalNums { get { return _TotalNums; } set { if (_TotalNums == value) return; _TotalNums = value; OnPropertyChanged(nameof(TotalNums)); } }
+        private int _TotalNums;
+
+        public int Progress { get { return _Progress; } set { if (_Progress == value) return; _Progress = value; OnPropertyChanged(nameof(Progress)); } }
+        private int _Progress;
+
+        public float Percent { get { return _Percent; } set { if (_Percent == value) return; _Percent = Math.Max(Math.Min(value, 50), 0); _Nums = (int)(TotalNums * Percent /100); OnPropertyChanged(nameof(Percent)); OnPropertyChanged(nameof(Nums)); } }
+        private float _Percent;
+
+        public int Nums { get { return _Nums; } set { if (_Nums == value) return; _Nums = Math.Max(Math.Min(value,TotalNums/2),0); _Percent = (float)Nums * 100 / Math.Max(TotalNums, 1); OnPropertyChanged(nameof(Percent)); OnPropertyChanged(nameof(Nums)); } }
+        private int _Nums;
+
+        public List<string> filelist;
+        #endregion
+
+        #region 方法
+        public Model()
+        {
+            
+        }
+
+        private int GetFilesCount(string path)
+        {
+            int count = 0;
+            count += Directory.GetFiles(path).Length;
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                count += GetFilesCount(folder);
+            }
+            return count;
+        }
+
+        public void GetFilesName(string path)
+        {
+            string[] templist;
+            templist = Directory.GetFiles(path);
+            foreach (string filename in templist)
+            {
+                filelist.Add(filename);
+            }
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                GetFilesName(folder);
+            }
+        }
+
+        public void GetRandomByGuid(int[] array)
+        {
+            int len = array.Length;
+            Random random = new Random(GetRandomSeedbyGuid());
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = random.Next(0, len);
+            }
+        }
+
+        public int GetRandomSeedbyGuid()
+        {
+            return new Guid().GetHashCode();
+        }
+        #endregion
+
+        #region INotifyPropertyChanged接口便利实现
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
     }
 }
