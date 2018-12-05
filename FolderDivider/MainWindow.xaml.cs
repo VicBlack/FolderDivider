a877308948ff4e2544b5d9926222aabc3e68c732
Vic Black <vic_black@163.com>
2018-12-05 11:37:33 +0800

    Init

---
+++ FolderDivider/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FolderDivider
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new Model();
+            this.Content = _Model;
+        }
+
+        private void On_SetInputPath_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null;
+        }
+
+        private void On_SetOutPath_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null;
+        }
+
+        private void On_Divide_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null;
+        }
+
+        private void OnSetInputPath_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void OnSetOutPath_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void OnDivide_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private Model _Model;
+    }
+}
